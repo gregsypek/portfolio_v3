7e1329f32a3c40d66bfa60c27fe7984f02315050
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-09 09:39:54 +0100

    add hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,82 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const skills = ["React ", "Node ", "Vue ", "Javascript"];
+
+const textVariants = {
+	initial: {
+		x: -500,
+		opacity: 0,
+	},
+	animate: {
+		x: 0,
+		opacity: 1,
+		transition: {
+			duration: 1,
+			staggerChildren: 0.1,
+		},
+	},
+};
+const sliderVariants = {
+	initial: {
+		x: "150%",
+	},
+	animate: {
+		x: "-220%",
+
+		transition: {
+			repeat: Infinity,
+			repeatType: "mirror",
+			delay: 2,
+			// repeatDelay: 2,
+			// loop: Infinity,
+			duration: 30,
+		},
+	},
+};
+
+const Hero = () => {
+	return (
+		<div className="hero">
+			<div className="wrapper">
+				<motion.div
+					className="textContainer"
+					variants={textVariants}
+					initial="initial"
+					animate="animate"
+				>
+					<motion.h1 variants={textVariants}>
+						Web Developer & Content Creator
+					</motion.h1>
+					<motion.h2 variants={textVariants}>
+						I design and create web pages and projects with attention to clarity
+						and details.
+					</motion.h2>
+
+					<motion.div variants={textVariants} className="buttons">
+						<motion.button variants={textVariants}>
+							See the Latest Works
+						</motion.button>
+						<motion.button variants={textVariants}>Contact Me</motion.button>
+					</motion.div>
+				</motion.div>
+			</div>
+			<div></div>
+			<motion.div
+				className="slidingTextContainer"
+				variants={sliderVariants}
+				initial="initial"
+				animate="animate"
+			>
+				{skills?.map((skill) => (
+					<strong key={skill}>{skill}</strong>
+				))}
+			</motion.div>
+			<div className="imageContainer">
+				<img src="/hero.png" alt="me" />
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import "./navbar.scss";
 import { BsTwitterX } from "react-icons/bs";
 import { LuLinkedin } from "react-icons/lu";
 import { LuGithub } from "react-icons/lu";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
@@ -11,16 +11,22 @@ const Navbar = () => {
 			{/* sidebar */}
 			<Sidebar />
 			<div className="wrapper">
-				<motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration: 0.5}}>GREG SYPEK</motion.span>
+				<motion.span
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
+				>
+					GRZEGORZ SYPEK
+				</motion.span>
 				<div className="social">
 					<a href="#">
-						<BsTwitterX size={24}/>
+						<BsTwitterX size={24} />
 					</a>
 					<a href="#">
-						<LuLinkedin size={24}/>
+						<LuLinkedin size={24} />
 					</a>
 					<a href="#">
-						<LuGithub size={24}/>
+						<LuGithub size={24} />
 					</a>
 				</div>
 			</div>
