ed96279fb676643d4690d66c59085f9ea6104ef6
gregsypek <47268138+gregsypek@users.noreply.github.com>
2024-01-10 20:08:55 +0100

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import "./app.scss"
+import "./app.scss";
+import About from "./components/about/About";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
-  return (
-  <div>
-    <section id="Homepage"><Navbar />
-    <Hero /></section>
-    <section id="About Me">Parallax</section>
-    <section id="Projects">Services </section>
-    <section>Portfolio1 </section>
-    <section>POrtfolio2 </section>
-    <section id="Experience"> Experience</section>
-    <section id="Contact"> Contact</section>
-  </div>)
-
+	return (
+		<div>
+			<section id="Homepage">
+				<Navbar />
+				<Hero />
+			</section>
+			<section id="About">
+				{/* <Parallax type="services" /> */}
+				<About />
+			</section>
+			<section id="Projects">
+				<Parallax type="portfolio" />{" "}
+			</section>
+			<section>Portfolio1 </section>
+			<section>POrtfolio2 </section>
+			<section id="Experience"> Experience</section>
+			<section id="Contact"> Contact</section>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,53 @@
+import "./about.scss";
+import about4 from "/about4.png";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useWindowSize } from "react-use";
+import { useRef } from "react";
+
+function About() {
+	const ref = useRef();
+	const { width: windowWidth } = useWindowSize();
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start start", "end start"], //animation, target, animation, -  animation start when target is on the top  and  animation is end wten top of the target reaches start
+	});
+	const transformText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+	const transformType =
+		windowWidth < 900 ? { x: transformText } : { y: transformText };
+
+	return (
+		<div className="about" ref={ref}>
+			<motion.div className="about__box about__box--1" style={transformType}>
+				<h1 className="about__heading">About Me</h1>
+			</motion.div>
+			<motion.div className="about__box about__box--2">
+				<motion.img
+					className="about__image"
+					src={about4}
+					alt="mauntain trail sign"
+				/>
+			</motion.div>
+			<motion.p className="about__text about__text--1">
+				I am a frontend developer with five years of programming experience,
+				specializing in React, React Native, and Vue.js. Lastly I have been
+				working as application programmer, using React Native, React and
+				Typescript on application for enginerrs, and as junior developer in
+				Rzeszów on a big e-commerce app.
+			</motion.p>
+			<motion.p className="about__text about__text--2">
+				I live in a small town of Mielec in Poland. I am a self-taught learner
+				who is passionate about computers and coding. In my spare time I love
+				listening to progressive rock, reading books and hiking in the
+				mountains.
+			</motion.p>
+			<motion.p className="about__text about__text--3">
+				I am willing <span>to cooperate </span>and continue my adventure with IT
+				and develop further in this direction.
+			</motion.p>
+			<motion.button className="about__btn btn">Contact</motion.button>
+			<motion.div className="mountains"></motion.div>
+		</div>
+	);
+}
+
+export default About;
